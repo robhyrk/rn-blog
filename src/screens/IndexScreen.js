faefe0cccb9dd23db7bb45bd2ee0ab8514def1e5
robhyrk <rob.hyrkiel@gmail.com>
2020-02-12 18:44:01 -0500

    feat: add blog post title

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useContext} from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext'
+// import { FlatList } from 'react-native-gesture-handler';
+
 
 const IndexScreen = () => {
+
+  const {data, addBlogPost} = useContext(BlogContext)
+
   return (
     <View style={styles.container}>
+      <Button title="Add Post" onPress={addBlogPost}/>
       <Text>Index Screen</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost)=>{
+          return blogPost.title
+        }}
+        renderItem={({item}) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   );
 }
